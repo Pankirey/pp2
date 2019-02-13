b413c89fa3deea880dd5debd68ec1c54e3f2bf3e
Pankirey <pankirey@gmail.com>
2019-02-13 22:01:06 +0600

    13.02.19

---
+++ Lab2/Task 3/Task 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class FileSysInfo
+    {
+        static void Main(string[] args)
+        {
+            System.IO.DriveInfo di = new System.IO.DriveInfo(@"C:\Users\Panki\Desktop\pp22");
+            System.IO.DirectoryInfo dirInfo = di.RootDirectory;
+            System.IO.FileInfo[] fileNames = dirInfo.GetFiles("*.*");
+
+            System.IO.DirectoryInfo[] dirInfos = dirInfo.GetDirectories("*.*");
+
+            foreach (System.IO.DirectoryInfo d in dirInfos)
+            {
+                Console.WriteLine(d.Name);
+            }
+
+        }
+    }
+}
